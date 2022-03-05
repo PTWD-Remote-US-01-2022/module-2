64d68296088d9eca9a421ea6d48c3180f050270b
sandrabosk <sandraboskovic@hotmail.com>
2022-03-05 19:11:25 +0100

    add class notes on fetch and promiseAll

--- promise-all/script.js
+++ promise-all/script.js
@@ -12,6 +12,9 @@ function getCountryDetails(name){
   .then(data => {
     console.log("Here is the country data: ", data[0][0].name);
 
+    // this is added outside of the class so you have it as a reference
+    // this is great example of messed up data - basically the return value is an array of arrays, not as we would expect an array of objests
+    // this means we have to loop through first and then through the second array
     const countryData = document.querySelector("h2");
 
     data.forEach(singleCountryAsArray => {
